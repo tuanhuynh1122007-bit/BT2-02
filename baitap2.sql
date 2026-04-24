18ccfed9ecfc9b62274bece71888f9dc67323185
tuanhuynh1122007-bit <tuanhuynh1122007@gmail.com>
2026-04-24 13:10:35 +0700

    Create database and customers table with constraints

--- baitap2.sql
+++ baitap2.sql
@@ -1,1 +1,15 @@
-SELECT * FROM baitap2.customers;
+CREATE DATABASE baitap2_ss2;
+USE baitap2_ss2;
+
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY,
+    fullname VARCHAR(100),
+    email VARCHAR(100),
+    age INT
+);
+
+ALTER TABLE customers
+MODIFY email VARCHAR(100) NOT NULL,
+ADD UNIQUE(email),
+MODIFY age INT NOT NULL,
+ADD CHECK(age > 0);
